bd07a38f98bf0d2259987909fff0fd08b40df82f
jansn <bob@firmhouse.nl>
2012-09-11 06:42:46 +0200

    Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeatTest::Application.config.secret_token = '635d925630a80f658f4cebb16ca2a45f62d37769bd65784a90db35a54b122dcbc70144404f803a855a52004d96831bd700adc4edd9fda907fdec38724494baed'
